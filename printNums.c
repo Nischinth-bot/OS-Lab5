f54e959661e1147c25199a72d66501447f1b5a51
Nischinth Murari <murarins@appstate.edu>
2020-02-26 22:30:56 -0500

    printNums working for 2 threads

--- printNums.c
+++ printNums.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "wrappers.h"
+pthread_cond_t myconvar = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+int global = 0;
+int numThreads = 0;
 
 /* getThreadCount
  * parse the command line arguments to get the number
@@ -13,14 +17,37 @@
  */
 int getThreadCount(int argc, char * argv[])
 {
-   int count;
-   if (argc < 2 || (count = atoi(argv[1])) <= 0)
-   {
-      printf("usage: printNums <thread count>\n");
-      exit(1);
-   }
-   return count;
+    int count;
+    if (argc < 2 || (count = atoi(argv[1])) <= 0)
+    {
+        printf("usage: printNums <thread count>\n");
+        exit(1);
+    }
+    numThreads = count;
+    return count;
 }
+
+/**
+ * printNumbers
+ * Thread function
+ */
+void* printNumbers(void* threadNumber){
+    int mult = 0;    
+    pthread_mutex_lock(&mymutex); //acquires the lock 
+    while(global < 100)
+    {
+        global = (int)threadNumber + 1 + (mult * numThreads); //updates global 
+        printf("Thread %d = %d\n", (int)threadNumber, global); //print updated global
+        mult ++; 
+        pthread_cond_signal(&myconvar); 
+        if(global < 100) pthread_cond_wait(&myconvar,&mymutex); //
+    }
+
+    pthread_mutex_unlock(&mymutex); //releases the lock
+    pthread_exit(NULL);
+}
+
+
 
 /*
  * main 
@@ -30,5 +57,15 @@ int getThreadCount(int argc, char * argv[])
  */
 int main (int argc, char *argv[])
 {
-   int threadCount = getThreadCount(argc, argv);
+    int threadCount = getThreadCount(argc, argv);
+    pthread_t threads[threadCount];
+    if (threadCount > 0)
+    {
+        int j;
+        for(j = 0; j < threadCount; j ++){
+            pthread_create(&threads[j], NULL, printNumbers, (void *) j);
+        }
+
+    }
+    pthread_exit(NULL);
 }
