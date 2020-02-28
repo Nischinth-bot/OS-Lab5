f91723ef2fd7f7780b11b03f739e4f717427ad05
Nischinth Murari <murarins@appstate.edu>
2020-02-28 01:03:52 -0500

    Almost working fully

--- printNums.c
+++ printNums.c
@@ -7,7 +7,8 @@ pthread_cond_t myconvar = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
 int global = 0;
 int numThreads = 0;
-
+int nextThread = 1;
+int first = 0;
 /* getThreadCount
  * parse the command line arguments to get the number
  * of threads to be created by this process.
@@ -31,19 +32,33 @@ int getThreadCount(int argc, char * argv[])
  * printNumbers
  * Thread function
  */
-void* printNumbers(void* threadNumber){
+void* printNumbers(void* threadNumber){ 
+    pthread_mutex_lock(&mymutex); //acquires the lock         
     int mult = 0;    
-    pthread_mutex_lock(&mymutex); //acquires the lock 
-    while(global < 100)
+    int threadNum = (int) threadNumber;
+   // printf("Thread %d got the signal\n", threadNum);
+    while(global < 10)
     {
-        global = (int)threadNumber + 1 + (mult * numThreads); //updates global 
-        printf("Thread %d = %d\n", (int)threadNumber, global); //print updated global
-        mult ++; 
-        pthread_cond_signal(&myconvar); 
-        if(global < 100) pthread_cond_wait(&myconvar,&mymutex); //
-    }
 
+        while((threadNum) != (nextThread - 1) && global < 10) {  
+          //   printf("Waiting on thread = %d %d\n", threadNum, nextThread);
+             pthread_cond_wait(&myconvar,&mymutex); 
+            // printf("Resuming thread = %d\n", threadNum);
+        }
+        if(global < 10) {    //Double check if global still needs updating
+            global = threadNum + 1 + (mult * numThreads);
+            fprintf(stderr,"Thread %d = %d\n", (int)threadNumber, global); 
+            nextThread ++; 
+            if(nextThread > numThreads) nextThread = 1; //Reset the nextThread varible, if necessary.
+            mult ++; 
+        }
+     //   printf("Work done for thread %d, sending signal\n",threadNum);
+        pthread_cond_signal(&myconvar); /**Thread finishes its work for this 
+                                        iteration and signals other threads waiting on convar.**/
+        pthread_mutex_unlock(&mymutex); //releases the lock
+    }
     pthread_mutex_unlock(&mymutex); //releases the lock
+    pthread_mutex_destroy(&mymutex);
     pthread_exit(NULL);
 }
 
@@ -65,7 +80,6 @@ int main (int argc, char *argv[])
         for(j = 0; j < threadCount; j ++){
             pthread_create(&threads[j], NULL, printNumbers, (void *) j);
         }
-
     }
     pthread_exit(NULL);
 }
