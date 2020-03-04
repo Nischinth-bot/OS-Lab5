8c222b3409e85a6fe9c51390b5f7df9aa0665e97
Nischinth Murari <murarins@appstate.edu>
2020-03-04 17:17:28 -0500

    Wrapper class and header file uploaded

--- wrappers.c
+++ wrappers.c
@@ -8,12 +8,12 @@
 #include "wrappers.h"
 
 /* unixError
-* Called when an error occurs to print an error message and exit.
-*/
+ * Called when an error occurs to print an error message and exit.
+ */
 void unixError(char * msg)
 {
-   fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-   exit(1);
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
 }
 
 /* Pthread_create
@@ -27,15 +27,116 @@ void unixError(char * msg)
  * new  thread  in  the  buffer  pointed to by thread.
  * On success, pthread_create() returns 0; on error, it returns an error  number,
  * and the contents of *thread are undefined.
-*/
+ */
 int Pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-                   void *(*start_routine)(void *), void *arg)
+        void *(*start_routine)(void *), void *arg)
 {
-   int rc = pthread_create(thread, attr, start_routine, arg);
-   if (rc != 0)
-   {
-      unixError("pthread_create error");
-   }
-   return rc;
+    int rc = pthread_create(thread, attr, start_routine, arg);
+    if (rc != 0)
+    {
+        unixError("pthread_create error");
+    }
+    return rc;
 }
 
+int Pthread_join(pthread_t thread, void** retval){
+    int rc = pthread_join(thread, retval); 
+    if (rc != 0)
+    {
+        unixError("pthread_join error");
+    }
+    return rc;
+}
+
+
+int Pthread_mutex_destroy(pthread_mutex_t* mutex){
+    int rc = pthread_mutex_destroy(mutex);
+    if (rc != 0)
+    {
+        unixError("pthread_mutex_destroy error");
+    }
+    return rc;
+
+}
+
+
+int Pthread_mutex_lock(pthread_mutex_t* mutex){
+    int rc = pthread_mutex_lock(mutex);
+    if (rc != 0)
+    {
+        unixError("pthread_mutex_lock error");
+    }
+    return rc;
+}
+
+
+
+int Pthread_mutex_unlock(pthread_mutex_t* mutex){
+    int rc = pthread_mutex_unlock(mutex);
+    if (rc != 0)
+    {
+        unixError("pthread_mutex_unlock error");
+    }
+    return rc;
+}
+
+int Pthread_cond_wait(pthread_cond_t * myCond, pthread_mutex_t * myMutex){
+
+    int rc = pthread_cond_wait(myCond, myMutex);
+    if (rc != 0)
+    {
+        unixError("pthread_cond_wait error");
+    }
+    return rc;
+}
+
+
+int Pthread_cond_signal(pthread_cond_t * myCond){
+
+    int rc = pthread_cond_signal(myCond);
+    if (rc != 0)
+    {
+        unixError("pthread_cond_signal error");
+    }
+    return rc;
+}
+
+
+int Sem_init(sem_t* sem, int a, int b){
+    int rc = sem_init(sem,a,b);
+    if(rc != 0)
+    {
+        unixError("sem_init error");
+    }
+    return rc;
+
+}
+
+int Sem_destroy(sem_t* sem){
+    int rc = sem_destroy(sem);
+    if(rc != 0)
+    {
+        unixError("sem_detroy error");
+    }
+    return rc;
+
+}
+
+int Sem_wait(sem_t* sem){
+    int rc = sem_wait(sem);
+    if(rc != 0)
+    {
+        unixError("sem_wait error");
+    }
+    return rc;
+
+}
+
+int Sem_post(sem_t* sem){
+    int rc = sem_post(sem);
+    if(rc != 0)
+    {
+        unixError("sem_post error");
+    }
+    return rc;
+}
--- wrappers.h
+++ wrappers.h
@@ -1,5 +1,13 @@
-
+#include<semaphore.h>
 void unixError(char * msg);
 
 int Pthread_create(pthread_t *thread, const pthread_attr_t *attr,
                    void *(*start_routine)(void *), void *arg);
+int Pthread_join(pthread_t thread, void** retval);
+int Pthread_mutex_destroy(pthread_mutex_t* myMutex);
+int Pthread_cond_wait(pthread_cond_t* myCond, pthread_mutex_t* myMutex);
+int Pthread_cond_signal(pthread_cond_t* myCond);
+int Sem_init(sem_t* sem, int a, int b);
+int Sem_wait(sem_t* sem);
+int Sem_post(sem_t* sem);
+int Sem_destroy(sem_t* sem);
